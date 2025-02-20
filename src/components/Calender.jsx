6584eb73fd5ae61dc7775ada9a5a1e42750adb69
Surender Pratap Singh <rk31151@gmail.com>
2025-02-20 12:44:49 +0530

    layout of app done

---
+++ src/components/Calender.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Calendar.css";
+
+const Calender = () => {
+    return <div className="calendar">Calender</div>;
+};
+
+export default Calender;
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import Weather from "./Weather";
+import Calender from "./Calender";
+import "./News.css";
 
 const News = () => {
-    return <div>news</div>;
+    return (
+        <div className="news">
+            <header className="news-header">header</header>
+            <div className="news-content">
+                <div className="navbar">
+                    <div className="user">user</div>
+                    <div className="categories">categories</div>
+                </div>
+                {/* News Section */}
+                <div className="news-section">
+                    <div className="headline">headline</div>
+                    <div className="news-grid">
+                        <div className="news-item">news item</div>
+                        <div className="news-item">news item</div>
+                        <div className="news-item">news item</div>
+                        <div className="news-item">news item</div>
+                    </div>
+                </div>
+                {/* End of News Section */}
+
+                {/* My Blogs */}
+                <div className="my-blogs">
+                    <div className="blog">blog</div>
+                    <div className="blog">blog</div>
+                    <div className="blog">blog</div>
+                    <div className="blog">blog</div>
+                </div>
+
+                {/* End of My Blogs */}
+                <div className="weather-calendar">
+                    <Weather />
+
+                    <Calender />
+                </div>
+            </div>
+            {/* Footer */}
+            <footer className="news-footer">
+                <div className="footer-content">footer content</div>
+            </footer>
+            {/* End of Footer */}
+        </div>
+    );
 };
 
 export default News;
